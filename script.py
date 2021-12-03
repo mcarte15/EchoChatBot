4e96bccdf7910f80924eff8aadb72877ee5b6e72
mcarte15 <63749701+mcarte15@users.noreply.github.com>
2021-12-03 00:34:40 -0500

    Create script.py
    Script for the EchoChat bot introduces itself, and explains to the user what the purpose of the chatbot is, and collects/repeats user input

---
+++ script.py
@@ -0,0 +1,6 @@
+greeting_message = "Welcome! This is my first chatbot project."
+print(greeting_message)
+chatbot_explanation = "This chatbot has been programmed to be an echo chatbot. It will repeat whatever you say."
+print(chatbot_explanation)
+stuff_to_echo = input("What would you like to say to the Echo Chatbot?")
+print("You said: " + stuff_to_echo)
